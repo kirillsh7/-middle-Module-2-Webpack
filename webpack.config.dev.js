b176a4131fe769584fc4447d52ced787a757fafe
Kirill Shlogov <fungoodtv@gmail.com>
2025-10-20 07:36:54 +0300

    feat: initial project

---
+++ webpack.config.dev.js
@@ -0,0 +1,20 @@
+const { merge } = require('webpack-merge')
+const ESLintPlugin = require("eslint-webpack-plugin")
+const commonConfig = require('./webpack.config.common')
+module.exports = merge(commonConfig, {
+	mode: 'development',
+	devtool: "inline-source-map",
+	devServer: {
+		port: 3000,
+		hot: true,
+		open: true,
+		devMiddleware: {
+			writeToDisk: true,
+		},
+	},
+	plugins: [new ESLintPlugin({
+		overrideConfigFile: '.eslintrc.js',
+		extensions: ['js', 'jsx'],
+	})],
+
+})
